97cf3c21f2066bbcb5416a2da57610fc9fc14417
sirasit <sirasit@siruxsolutions.com>
2022-01-16 13:43:46 -0800

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer(){
+    var date = new Date();
+    var year = date.getFullYear();
+    return <p>Copyright &copy {year}. Created By Sirux</p>
+}
+
+export default Footer;
