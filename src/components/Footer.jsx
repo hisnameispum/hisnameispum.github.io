83e15afe81d66ce976ddc60257dde2b4b6e769b9
sirasit <sirasit@siruxsolutions.com>
2022-01-18 12:42:23 -0800

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     var date = new Date();
     var year = date.getFullYear();
-    return <footer>Copyright &copy; {year}. Created By Sirux</footer>
+    return <footer>Copyright &copy; {year}. <br/>Created By Sirasit Thitirattanakorn</footer>
 }
 
 export default Footer;
